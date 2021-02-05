378928cb886e871a7cee66c4d08cc07f44bde79c
Kevin Wegener Antolinez <48557454+kevinwnb@users.noreply.github.com>
2021-02-05 14:19:38 -0400

    Update 2020_10_25_214937_create_products_table.php

--- database/migrations/2020_10_25_214937_create_products_table.php
+++ database/migrations/2020_10_25_214937_create_products_table.php
@@ -14,7 +14,7 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->collation = 'utf8mb4_unicode_ci';
+            $table->collation('utf8mb4_unicode_ci');
             $table->id();
             $table->string('name');
             $table->text('description');
