22b5174cb612bde356e0cb237478f0abbbbdcc7f
Kevin Wegener Antolinez <48557454+kevinwnb@users.noreply.github.com>
2021-02-14 18:30:03 -0400

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
-            $table->string('phone');
+            $table->string('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
